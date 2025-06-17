e31ccddf54e70c036b1e4b840feac9499f2421a8
saint-broseph <sahutanishq06@gmail.com>
2025-06-17 17:47:38 +0530

    Create snapshots_indicators.py

---
+++ utils/snapshots_indicators.py
@@ -0,0 +1,7 @@
+SNAPSHOT_INDICATORS = {
+    "GDP Growth (%)": "NY.GDP.MKTP.KD.ZG",
+    "Inflation (%)": "FP.CPI.TOTL.ZG",
+    "Interest Rate (%)": "FR.INR.RINR",
+    "Unemployment Rate (%)": "SL.UEM.TOTL.ZS",
+    "Population": "SP.POP.TOTL"
+}
